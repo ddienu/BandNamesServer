246c1988830636ffe66ba9375c6b3668001bfd52
ddienu <rockdann@hotmail.com>
2022-11-29 01:47:03 -0500

    Update: package.json, update: index.html, update: index,js
    Update package.json installing the socket.io from the terminal.
    Update index.html creating the script source from the socket.io/socket.io.js and another script with a variable called socket that create a new instance of the io, if the word connect if finded will appear a console log saying that the socket is connected to the server, and if the word disconnect is finded will appear a console log saying that the socket lost the connection with the server.
    Update index.js adding the use properties of socket.io, creating the node server, the socket messages if the user is on or off and changing from app to server.

--- index.js
+++ index.js
@@ -4,15 +4,32 @@ const express = require('express');
 const path    = require('path');
 require('dotenv').config();
 
+//Express App
+
 const app = express();
 
+//Node Server
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Socket Messages
+
+io.on('connection', client => {
+    console.log('Usuario conectado')
+
+    client.on('disconnect', () => { 
+        
+        console.log('Usuario desconectado')
+     });
+  });
 
 const publicPath = path.resolve( __dirname, 'public');
 app.use ( express.static( publicPath ));
 
 
 
-app.listen (  process.env.PORT, ( err )=>{
+server.listen (  process.env.PORT, ( err )=>{
 
     if ( err ) throw new Error( err );
 
